72721ad2a885580898309f761c3346ec41bec9dd
kristenSilva <kristensilvarobles@gmail.com>
2021-05-05 10:05:57 -0700

    can't connect api with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch
+// } from 'react-router-dom';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div>Data should go here</div>
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Data should go here</div>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/courses')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        data: data
+      });
+      console.log('data from api:', data);
+    });
+  }
+
+  // render() {
+  //   return(
+  //     <div>{[this.state.data]}</div>
+  //   )
+  // }
+
 }
-
 export default App;
